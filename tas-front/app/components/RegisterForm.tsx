0dae9c098a7277642ee108c62962748b2f3a4f44
cmanuelcabbage <cmanuel.cabbage@gmail.com>
2026-06-24 23:09:34 -0500

    Register form

--- tas-front/app/components/RegisterForm.tsx
+++ tas-front/app/components/RegisterForm.tsx
@@ -17,7 +17,7 @@ export default function RegisterForm() {
     setSuccess(false);
 
     try {
-      const res = await fetch('http://localhost:3000/auth/register', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
